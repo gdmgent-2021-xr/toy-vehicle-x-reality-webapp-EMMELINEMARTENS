5eb10c7655528c12fa800e8690ef3f16c5e14e52
Emmeline Martens <50316143+EMMELINEMARTENS@users.noreply.github.com>
2020-10-26 18:15:51 +0100

    Add files via upload

--- src/App/index.js
+++ src/App/index.js
@@ -1,30 +1,56 @@
 import React, { Suspense } from "react";
-// import { Canvas } from "react-three-fiber";
+import { Canvas } from "react-three-fiber";
 import { FlyControls, OrbitControls } from "@react-three/drei";
 import { DefaultXRControllers, VRCanvas } from "@react-three/xr";
+import { useControl } from "react-three-gui";
+
+import * as THREE from "three";
+
 
 import {
   GlTransmissionFormat,
   Lighting,
   Suzanne,
+  Pano,
   Tripod,
+  Car,
 } from "App/components";
 import "./styles.css";
 
+
 export default () => {
+
+ const showAxesHelper = useControl("Axes Helper", {
+    group: "Helpers",
+    type: "boolean",
+    value: true,
+  });
+  const showGridHelper = useControl("Grid Helper", {
+    group: "Helpers",
+    type: "boolean",
+    value: true,
+  });
+
   return (
-    <VRCanvas invalidateFrameloop={false}>
+    <Canvas>
+      <group>
+      {showAxesHelper && <axesHelper />}
+        {showGridHelper && <gridHelper args={[10, 10, 0xffffff, 0x333333]} />}
+      
       <OrbitControls enablePan={true} enableRotate={true} enableZoom={true} />
       <FlyControls dragToLook={true} />
       {false && <DefaultXRControllers />}
-      {false && <Lighting />}
-      {true && <Tripod />}
-      {false && (
+      </group>
+
+      {true && <Lighting />}
+      {false && <Tripod />}
+      {true && (
         <Suspense fallback={null}>
-          <Suzanne />
-          <GlTransmissionFormat />
+          {/* <Suzanne /> */}
+          {true && <Car />}
+          {/* <GlTransmissionFormat /> */}
         </Suspense>
       )}
-    </VRCanvas>
+    </Canvas>
   );
 };
