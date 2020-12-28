5fc7ac5317da651217090a142e6b5f8c9439416e
pgmgent-EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2020-12-28 16:40:40 +0100

    wheels_of_te_car_spins

--- src/App/components/Car/index.js
+++ src/App/components/Car/index.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { useFrame } from "react-three-fiber";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from 'three';
-import { animated, useSpring } from "react-spring/three";
+import { a, useSpring } from "react-spring/three";
 import{proxy, useProxy} from "valtio";
 
 import Car from './toy.glb';
@@ -30,29 +30,38 @@ export default ({props, state, snap}) => {
 const [hovered, setHover] = useState(null);
 
   // const cargltf = useGLTF(Car, true);
-  const carRef = useRef()
+  const carRef = useRef();
+  const frontwheelsRef = useRef();
+  const backwheelsRef = useRef();
+  const candylabtextRef=useRef();
   const { nodes, materials } = useGLTF(Car, true)
 
   // cargltf.scene.children[1].color.set("red");
+const Ref = (ref) => {
+useFrame(()=> {
+ref.current.rotation.y += 0.04;
+})
+}
 
-  console.log(nodes);
-  console.log(materials);
+
 
   // useState
   // const [hovered, setHover] = useState(null);
 // const snap = useProxy(state);
-
+Ref(frontwheelsRef);
+Ref(backwheelsRef);
 
 
   return (
     <>
+    
 <group ref={carRef} {...props} dispose={null} 
 scale={[0.4,0.4,0.4]}
 position={[-9,-3.9,-5.5]}
 onPointerOver={(e)=> (setHover(e.object.material.name))}
 onPointerOut={(e) => setHover(null)}
 >
-    <primitive ref={carRef} object={nodes.Scene} />
+    {/* <primitive ref={carRef} object={nodes.Scene} /> */}
 
  <mesh
         material={materials.windowcolor}
@@ -60,6 +69,12 @@ onPointerOut={(e) => setHover(null)}
         position={[-0.05, 3.87, 0.47]}
         scale={[2.01, 1.21, 2.59]}
         material-color={snap.items.windowcolor}
+      />
+      <mesh
+        material={materials.lightgrey}
+        geometry={nodes.sireneeiland.geometry}
+        position={[-0.85, 5.16, 0.12]}
+        scale={[0.78, -0.51, 0.73]}
       />
 
       <mesh
@@ -69,82 +84,40 @@ onPointerOut={(e) => setHover(null)}
         scale={[0.73, -0.86, 0.65]}
         material-color={snap.items.lightred}
       />
-      <group position={[-0.01, 1.79, 0.05]} scale={[7.62, 0.83, 2.91]}>
-        <mesh material={materials.bodycolor} geometry={nodes.carbody_1_0.geometry}  material-color={snap.items.bodycolor}/>
-        <mesh material={materials.lightstripered} geometry={nodes.carbody_2_1.geometry} material-color={snap.items.lightstripered} />
-      </group>
-      {/* <mesh
+      <mesh
         material={materials.white}
         geometry={nodes.plakkaten.geometry}
         position={[-0.01, 1.83, 2.99]}
         rotation={[-3.13, 0, 0]}
         scale={[3.65, 0.72, 0.03]}
-      /> */}
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab.geometry}
-        position={[-5.4, 1.59, 3]}
-        rotation={[-1.61, -0.25, 3.13]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
       />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab001.geometry}
-        position={[-5.7, 1.59, -2.89]}
-        rotation={[-1.52, 0.25, -0.05]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab002.geometry}
-        position={[2.51, 1.6, -3.07]}
-        rotation={[-1.52, 0.25, -0.05]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab003.geometry}
-        position={[2.72, 1.66, 3.17]}
-        rotation={[-1.61, -0.25, 3.13]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab004.geometry}
-        position={[-5.62, 0.8, 3.26]}
-        rotation={[1.3, 0.22, 0.05]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab005.geometry}
-        position={[-5.47, 0.8, -3.14]}
-        rotation={[1.85, -0.22, -3.06]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab006.geometry}
-        position={[2.74, 0.81, -3.33]}
-        rotation={[1.85, -0.22, -3.06]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-      <mesh
-        material={materials.candylabcolor}
-        geometry={nodes.candylab007.geometry}
-        position={[2.5, 0.87, 3.43]}
-        rotation={[1.3, 0.22, 0.05]}
-        scale={[0.48, 0.7, 0.48]}
-        material-color={snap.items.candylabcolor}
-      />
-     
+      <group position={[-0.01, 1.79, 0.05]} scale={[7.62, 0.83, 2.91]}>
+        <mesh material={materials.bodycolor} geometry={nodes.carbody_1_0.geometry}  material-color={snap.items.bodycolor}/>
+        <mesh material={materials.lightstripered} geometry={nodes.carbody_2_1.geometry} material-color={snap.items.lightstripered} />
+      </group>
+
+      
+        <a.mesh
+        ref={frontwheelsRef}
+        material={materials.darkgrey}
+        geometry={nodes.wheels.geometry}
+        position={[2.67, 1.2, 2.97]}
+        rotation={[1.55, 0.43, -3.14]}
+        scale={[0.79, 0.18, 0.79]}
+        material-color={snap.items.darkgrey}
+       />
+        
+        <a.mesh
+        ref={backwheelsRef}
+        material={materials.darkgrey}
+        geometry={nodes.wheel.geometry}
+        position={[-5.5, 1.2, 2.8]}
+        rotation={[1.55, 0.43, -3.14]}
+        scale={[0.79, 0.18, 0.79]}
+        material-color={snap.items.darkgrey}
+       
+        />
+
       <mesh
         material={materials.yellow}
         geometry={nodes.shield.geometry}
@@ -152,15 +125,36 @@ onPointerOut={(e) => setHover(null)}
         scale={[0.5, 0.5, 0.05]}
         material-color={snap.items.yellow}
       />
+      
+        <mesh
+        material={nodes.wheelplate.material}
+        geometry={nodes.wheelplate.geometry}
+        position={[-5.54, 1.19, 3]}
+        rotation={[-1.58, 0.15, -0.03]}
+        scale={[0.39, 0.02, 0.39]}
+      />
       <mesh
-        material={materials.darkgrey}
-        geometry={nodes.wheels.geometry}
-        position={[2.67, 1.2, 2.97]}
-        rotation={[1.55, 0.43, -3.14]}
-        scale={[0.79, 0.18, 0.79]}
-        material-color={snap.items.darkgrey}
-        />
-    
+        material={nodes.wheelplate001.material}
+        geometry={nodes.wheelplate001.geometry}
+        position={[2.64, 1.22, 3.15]}
+        rotation={[-1.58, 0.15, -0.03]}
+        scale={[0.39, 0.02, 0.39]}
+      />
+      <mesh
+        material={materials.wheelplatecolor}
+        geometry={nodes.littlewheelplate1.geometry}
+        position={[-5.52, 1.19, 3.04]}
+        rotation={[-1.58, 0.15, -0.03]}
+        scale={[0.13, 0.01, 0.13]}
+      />
+      <mesh
+        material={materials.wheelplatecolor}
+        geometry={nodes.littlewheelplate2.geometry}
+        position={[2.64, 1.25, 3.23]}
+        rotation={[-1.58, 0.15, -0.03]}
+        scale={[0.13, 0.01, 0.13]}
+      />
+      
     </group>
     </>
   )
