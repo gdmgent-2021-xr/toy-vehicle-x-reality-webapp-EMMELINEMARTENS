57555fde903c81c25cd7930b5a170062b183c5f8
pgmgent-EMMELINEMARTENS <emmemart1@student.arteveldehs.be>
2020-12-31 11:17:52 +0100

    deploy

---
+++ docs/precache-manifest.d8e4b3e4806518fe383ca2c649068a24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7507c3cb911440c97cf0039cf6cf895e",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/index.html"
+  },
+  {
+    "revision": "af4b272eb950e697fa38",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/css/main.d9654e4e.chunk.css"
+  },
+  {
+    "revision": "52eb7a912998d31eaea5",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/js/2.812f63ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/js/2.812f63ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4b272eb950e697fa38",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/js/main.dddbd8c8.chunk.js"
+  },
+  {
+    "revision": "b738154ae3864cdaa3d1",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/js/runtime-main.c157991f.js"
+  },
+  {
+    "revision": "c4d9f17fe5a6aefd2a4128d6d2f332e2",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/media/bg.c4d9f17f.gltf"
+  },
+  {
+    "revision": "73580d603f4000f93d50286c5bd5faaf",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/media/buildings.73580d60.gltf"
+  },
+  {
+    "revision": "9e476063154c5e8ccb15cc6d821c1876",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/media/thief.9e476063.gltf"
+  },
+  {
+    "revision": "e40d28cd5c216a2d8e33b6b3e4a04438",
+    "url": "/toy-vehicle-x-reality-webapp-EMMELINEMARTENS/static/media/toy.e40d28cd.glb"
+  }
+]);
